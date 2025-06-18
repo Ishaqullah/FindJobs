4dab18424035ed5084c2c88b3490a1e28bc4e396
Ishaqullah <ishaq.siddiqui1401@gmail.com>
2025-06-18 12:06:16 +0500

    fix : center aligned all the comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import SearchForm from './components/SearchForm';
@@ -9,32 +8,35 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-      
-      <div className="flex">
-        <Sidebar user={mockUser} />
-        
-        <main className="flex-1 p-8">
-          <div className="max-w-5xl">
-            <SearchForm />
-            
-            <JobSection 
-              title="Featured Jobs" 
-              jobs={featuredJobs} 
-              seeAllLink="See Featured Jobs"
-            />
-            
-            <JobSection 
-              title="Recommended Jobs" 
-              jobs={recommendedJobs} 
-              seeAllLink="See Recommended Jobs"
-            />
-            <JobSection 
-              title="Latest Jobs" 
-              jobs={latestJobs} 
-              seeAllLink="See Latest Jobs"
-            />
-          </div>
-        </main>
+
+      <div className="flex justify-center">
+        <div className="flex w-full max-w-7xl">
+          <Sidebar user={mockUser} />
+
+          <main className="flex-1 p-8">
+            <div className="max-w-5xl mx-auto">
+              <SearchForm />
+
+              <JobSection 
+                title="Featured Jobs" 
+                jobs={featuredJobs} 
+                seeAllLink="See Featured Jobs"
+              />
+
+              <JobSection 
+                title="Recommended Jobs" 
+                jobs={recommendedJobs} 
+                seeAllLink="See Recommended Jobs"
+              />
+
+              <JobSection 
+                title="Latest Jobs" 
+                jobs={latestJobs} 
+                seeAllLink="See Latest Jobs"
+              />
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
